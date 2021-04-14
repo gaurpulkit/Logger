2de77be0b6a7543fe6dd8548017157cefa96354c
Pulkit Gaur <30400626+Zuj3brusu@users.noreply.github.com>
2021-04-14 13:47:12 +0530

    Update setup.py
    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Logger-Zuj3brusu",
-    version="1.0.1",
+    version="2.0",
     author="Zuj3brusu",
     description="Create logs on Discord, Telegram or a local file",
     long_description=long_description,
